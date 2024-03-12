2553c4c0fea0f26fdcc1a955984ae488727538de
truongvanhot <truongvanhot98@gmail.com>
2024-03-12 23:00:21 +0900

    evaluate downstream task

--- extra_tools/evaluate.sh
+++ extra_tools/evaluate.sh
@@ -1,5 +1,5 @@
 GPUS=4
 PORT=1998
-CONFIG=projects/configs/unipad_abl/finetune_cfg_full.py
-CHECKPOINT=work_dirs/finetune_cfg_full/epoch_12.pth
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT ./extra_tools/test.py $CONFIG $CHECKPOINT --eval mAP  
+CONFIG=projects/configs/exp/uvtr_convnext_s_vs0.1_c128_finetune.py
+CHECKPOINT=work_dirs/uvtr_convnext_s_vs0.1_c128_finetune/epoch_12.pth
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT ./extra_tools/test.py $CONFIG $CHECKPOINT --eval mAP
